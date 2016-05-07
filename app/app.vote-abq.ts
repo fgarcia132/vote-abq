09338bab2d9a8c3279e70ab3d35f78b7bbc82b34
Francisco Garcia <ciscog11@msn.com>
2016-05-07 14:08:29 -0600

    added base to index.html

--- app/app.vote-abq.ts
+++ app/app.vote-abq.ts
@@ -2,12 +2,17 @@ import {Component} from "angular2/core";
 import {VotingList} from "./voting-list";
 import {HTTP_PROVIDERS} from "angular2/http";
 import {VotingService} from "./voting.service";
+import {RouteConfig, ROUTER_PROVIDERS, ROUTER_DIRECTIVES} from "angular2/router";
 
 @Component({
 	selector: "vote-abq",
 	templateUrl: "app/templates/splash-page.html",
-	directives: [VotingList],
-	providers: [HTTP_PROVIDERS, VotingService]
+	directives: [ROUTER_DIRECTIVES, VotingList],
+	providers: [HTTP_PROVIDERS, ROUTER_PROVIDERS, VotingService]
 })
+@RouteConfig([
+	{path: "/", name:"VoteAbqSplash", component: VoteAbq},
+	{path: "/voting/:id", name: "VoteAbqDetail", component: VotingList}
+])
 
 export class VoteAbq {}
